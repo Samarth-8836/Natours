5e6f6c02dedee4db4ccb819416d6dbf86d151c36
Samarth Patel <psamarth8836@gmail.com>
2020-11-05 15:03:36 +0530

    Added routing with better file structure

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 app.use(express.json());
+app.use(morgan('dev'));
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ key: 'value' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
-
-
-// READ TOUR FILE
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// GET ALL TOURS
-
-app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours: tours } });
+app.use((req, res, next) => {
+  console.log('hello from the middleware');
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-// URL PRACTICE
-
-app.get('/api/v1/tours/:id/:id2', (req, res) => {
-  console.log(req.params);
-  res.status(200).send('OK');
-});
-
-
-// POST A NEW TOUR
-
-app.post('/api/v1/tours', (req, res) => {
-  //   console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
-});
-
-
-// LISTEN TO A PORT
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server Running on port ${port}`);
-});
+module.exports = app;
